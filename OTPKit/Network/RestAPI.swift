496aca9f108deec829c57e2422178050ba0b0394
hilmyveradin <hilmyveradin@gmail.com>
2024-06-25 12:10:55 +0700

    add swiftlint auto fix after swiftformat

--- OTPKit/Network/RestAPI.swift
+++ OTPKit/Network/RestAPI.swift
@@ -50,7 +50,7 @@ public actor RestAPI {
             URLQueryItem(name: "mode", value: mode),
             URLQueryItem(name: "arriveBy", value: arriveBy ? "true" : "false"),
             URLQueryItem(name: "maxWalkDistance", value: String(maxWalkDistance)),
-            URLQueryItem(name: "wheelchair", value: wheelchair ? "true" : "false"),
+            URLQueryItem(name: "wheelchair", value: wheelchair ? "true" : "false")
         ]
 
         let request = URLRequest(url: components.url!)
--- scripts/swiftlint.sh
+++ scripts/swiftlint.sh
@@ -4,7 +4,7 @@ export PATH="$PATH:/opt/homebrew/bin"
 if [ "$CI" = true ]; then
   echo "skipping swiftlint because in GitHub Actions it runs in a separate job."
 elif which swiftlint >/dev/null; then
-  swiftlint
+  swiftlint --fix && swiftlint
 else
   echo "warning: SwiftLint not installed, download from https://github.com/realm/SwiftLint or via Homebrew."
 fi
