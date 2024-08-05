7c45a62c0b07f5b36576b0fe5a9756d437c9a736
Aaron Brethorst <aaron@brethorsting.com>
2024-08-05 14:30:31 -0700

    DRY up some code and extract reusable functions into separate modules

--- OTPKit/Features/TripPlanner/TripPlannerSheetView.swift
+++ OTPKit/Features/TripPlanner/TripPlannerSheetView.swift
@@ -13,110 +13,15 @@ public struct TripPlannerSheetView: View {
 
     public init() {}
 
-    private func formatTimeDuration(_ duration: Int) -> String {
-        if duration < 60 {
-            return "\(duration) second\(duration > 1 ? "s" : "")"
-        } else if duration < 3600 {
-            let minutes = Double(duration) / 60
-            return String(format: "%.1f min", minutes)
-        } else {
-            let hours = Double(duration) / 3600
-            return String(format: "%.1f hours", hours)
-        }
-    }
-
-    private func formatDistance(_ distance: Int) -> String {
-        if distance < 1000 {
-            return "\(distance) meters"
-        } else {
-            let miles = Double(distance) / 1609.34
-            return String(format: "%.1f miles", miles)
-        }
-    }
-
-    private func formatBusSchedule(_ date: Date) -> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "h:mm a"
-        dateFormatter.amSymbol = "AM"
-        dateFormatter.pmSymbol = "PM"
-
-        let formattedTime = dateFormatter.string(from: date)
-
-        return "Bus scheduled at \(formattedTime)"
-    }
-
-    private func generateTramView(leg: Leg) -> some View {
-        HStack(spacing: 4) {
-            Text(leg.route ?? "")
-                .padding(.horizontal, 8)
-                .padding(.vertical, 4)
-                .background(Color.blue)
-                .foregroundStyle(.foreground)
-                .font(.caption)
-                .clipShape(RoundedRectangle(cornerRadius: 4))
-
-            Image(systemName: "tram")
-                .foregroundStyle(.foreground)
-        }.frame(height: 40)
-    }
-
-    private func generateBusView(leg: Leg) -> some View {
-        HStack(spacing: 4) {
-            Text(leg.route ?? "")
-                .padding(.horizontal, 8)
-                .padding(.vertical, 4)
-                .background(Color.green)
-                .foregroundStyle(.foreground)
-                .font(.caption)
-                .clipShape(RoundedRectangle(cornerRadius: 4))
-
-            Image(systemName: "bus")
-                .foregroundStyle(.foreground)
-        }
-        .frame(height: 40)
-    }
-
-    private func generateWalkView(leg: Leg) -> some View {
-        HStack(spacing: 4) {
-            Image(systemName: "figure.walk")
-            Text(formatTimeDuration(leg.duration))
-        }
-        .padding(.horizontal, 8)
-        .padding(.vertical, 4)
-        .background(Color.gray.opacity(0.2))
-        .foregroundStyle(.gray)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        .frame(height: 40)
-    }
-
-    private func generateDefaultView(leg: Leg) -> some View {
-        HStack(spacing: 4) {
-            Image(systemName: "questionmark.circle")
-                .foregroundStyle(.foreground)
-            Text(formatTimeDuration(leg.duration))
-        }
-        .padding(.horizontal, 8)
-        .padding(.vertical, 4)
-        .background(Color.gray.opacity(0.2))
-        .foregroundStyle(.gray)
-        .clipShape(RoundedRectangle(cornerRadius: 8))
-        .frame(height: 40)
-    }
-
     private func generateLegView(leg: Leg) -> some View {
         Group {
             switch leg.mode {
-            case "TRAM":
-                generateTramView(leg: leg)
-
-            case "BUS":
-                generateBusView(leg: leg)
-
+            case "BUS", "TRAM":
+                ItineraryLegVehicleView(leg: leg)
             case "WALK":
-                generateWalkView(leg: leg)
-
+                ItineraryLegWalkView(leg: leg)
             default:
-                generateDefaultView(leg: leg)
+                ItineraryLegUnknownView(leg: leg)
             }
         }
     }
@@ -132,11 +37,11 @@ public struct TripPlannerSheetView: View {
                     }, label: {
                         HStack(spacing: 20) {
                             VStack(alignment: .leading) {
-                                Text(formatTimeDuration(itinerary.duration))
+                                Text(Formatters.formatTimeDuration(itinerary.duration))
                                     .font(.title)
                                     .fontWeight(.bold)
                                     .foregroundStyle(.foreground)
-                                Text(formatBusSchedule(itinerary.startTime))
+                                Text(Formatters.formatBusSchedule(itinerary.startTime))
                                     .foregroundStyle(.gray)
 
                                 FlowLayout {
