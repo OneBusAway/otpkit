d5f6d87bb22b9b5704ebde98f1bbb7be7b72fb24
Mohamed Sliem <mohamedmostafa191299@gmail.com>
2025-03-21 17:12:05 +0200

    NoResultView: Create a reusable view for no result state

--- Sources/OTPKit/Features/OriginDestination/NoResultsView.swift
+++ Sources/OTPKit/Features/OriginDestination/NoResultsView.swift
@@ -7,12 +7,36 @@
 
 import SwiftUI
 
-struct SwiftUIView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct NoResultsView: View {
+    
+    private let iconName: String
+    private let title: String
+    private let subtitle: String
+    
+    init(iconName: String, title: String, subtitle: String) {
+        self.iconName = iconName
+        self.title = title
+        self.subtitle = subtitle
     }
-}
-
-#Preview {
-    SwiftUIView()
+    
+    var body: some View {
+        VStack {
+            Image(systemName: iconName)
+                .resizable()
+                .frame(width: 50, height: 50)
+                .font(.largeTitle)
+                .foregroundColor(.secondary)
+                .padding()
+            
+            Text(title)
+                .font(.title)
+                .foregroundColor(.black)
+                .padding()
+            
+            Text(subtitle)
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                .padding()
+        }
+    }
 }
