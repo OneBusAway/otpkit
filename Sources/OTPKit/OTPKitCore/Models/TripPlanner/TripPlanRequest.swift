0c4a3354bb7f154b8139e60b73c5dc9a109ce059
Manu R <manurajbhar12@gmail.com>
2025-07-21 00:27:47 +0530

    Update OTPKitCore

--- Sources/OTPKit/OTPKitCore/Models/TripPlanner/TripPlanRequest.swift
+++ Sources/OTPKit/OTPKitCore/Models/TripPlanner/TripPlanRequest.swift
@@ -124,45 +124,7 @@ public struct TripPlanRequest: Codable, Hashable {
     }
 }
 
-/// Represents different transportation modes available for trip planning
-public enum TransportMode: String, CaseIterable, Codable {
-    /// Public transit (bus, train, etc.)
-    case transit = "TRANSIT"
-    /// Walking
-    case walk = "WALK"
-    /// Bicycling
-    case bike = "BIKE"
-    /// Driving
-    case car = "CAR"
-    
-    /// Human-readable description of the transport mode
-    public var displayName: String {
-        switch self {
-        case .transit:
-            return "Transit"
-        case .walk:
-            return "Walk"
-        case .bike:
-            return "Bike"
-        case .car:
-            return "Drive"
-        }
-    }
-    
-    /// System image name for the transport mode
-    public var systemImageName: String {
-        switch self {
-        case .transit:
-            return "bus"
-        case .walk:
-            return "figure.walk"
-        case .bike:
-            return "bicycle"
-        case .car:
-            return "car"
-        }
-    }
-}
+
 
 // MARK: - CLLocationCoordinate2D Codable Extension
 
