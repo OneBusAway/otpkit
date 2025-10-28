d23bfe9d46ff8b8fe74ae2aab9795a6354225f45
Aaron Brethorst <aaron@brethorsting.com>
2025-10-28 16:20:21 -0700

    Delete two unused test files

--- OTPKitDemoTests/OTPKitTests.swift
+++
@@ -1,7 +0,0 @@
-import Testing
-@testable import OTPKit
-
-@Test func example() async throws {
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    #expect(true == true)
-}
